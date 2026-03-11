36bdbe3160228a3fe6f12411eae3371f2a17468b
Subhrabaran P <subhrabaranpanigrahi@gmail.com>
2026-03-12 03:03:45 +0530

    fix: filter users to retrieve only those with non-null emails in get_all_emails function

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,11 +1,11 @@
 from src.db.session import SessionLocal
 from src.db.models import User
 
-
+"""Fetches emails from the neon auth db to send job alerts to"""
 def get_all_emails() -> list[str]:
     db = SessionLocal()
     try:
-        users = db.query(User).all()
+        users = db.query(User).filter(User.email != None).all()
         return [u.email for u in users]
     finally:
         db.close()
